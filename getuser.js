d2b720faf6e484cae72f621755af3a4e2f80d645
rizkyrajitha <rajithagunathilake@gmail.com>
2021-05-29 22:27:02 +0530

    minor corrections

--- getuser.js
+++ getuser.js
@@ -7,36 +7,23 @@ const client = new Client({
   connectionString: connectionString,
 });
 
-client
-  .query({ text: `SELECT * FROM users`, values: [] })
-  .then((resolve) => {
-    console.log(resolve);
-  })
-  .catch((reject) => {
-    console.log(reject);
-  });
+async function getUser() {
+  let script = `SELECT * FROM users`;
+  await client.connect();
+  console.log("connected to database");
+  let query = {
+    text: script,
+    values: [],
+  };
+  console.log(query);
+  let res = await client.query(query);
+  console.log(res.command);
+  console.log(res.rowCount);
+  console.log(res.rows);
+  await client.end();
+  console.log("disconnected from database");
+}
 
-// async function getUser() {
-//   let script = `SELECT * FROM users`;
-
-//   let query = {
-//     text: script,
-//     values: [],
-//   };
-
-//   try {
-//     console.log(query);
-//     let res = await client.query(query);
-//     console.log(res.command);
-//     console.log(res.rowCount);
-//     console.log(res.rows);
-//     await client.end();
-//     console.log("disconnected from database");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// getUser()
-//   .then(() => process.exit())
-//   .catch((err) => console.error(err));
+getUser()
+  .then(() => process.exit())
+  .catch((err) => console.error(err));
